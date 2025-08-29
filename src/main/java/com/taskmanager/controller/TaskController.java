06ad18d3a30e1c6011d58fcad9f6eff34408e013
Shilpita Biswas <biswas.shilpita@gmail.com>
2025-08-29 10:49:08 -0700

    Fix GitHub Actions pipeline issues: PMD violations, security scan JAVA_HOME, and Docker build

--- src/main/java/com/taskmanager/controller/TaskController.java
+++ src/main/java/com/taskmanager/controller/TaskController.java
@@ -12,8 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.List;
 
 @Controller
